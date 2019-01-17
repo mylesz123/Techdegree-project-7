1e2131736218c34a1857ffdc2288a0a2d8b67f5b
Myles Y <39877233+mylesz123@users.noreply.github.com>
2019-01-17 13:07:15 -0500

    make Gallery.js file to show photos

---
+++ react_gallery_app-v1/react_gallery_app-v1/src/components/Gallery.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Image from '../components/Image';
+import NoResults from '../components/NoResults';
+
+//props passed from App
+const Gallery = (props) => {
+
+    let result = props.data;
+    console.log(result);
+
+    //if no results, show no results component, else return results inside ul
+    if(result.length = 0) {
+      return (
+        <div>
+          <NoResults /> //No Results component
+        </div>
+      )
+    }
+    else if(result.length > 0) {
+      let img = result.map(r =>
+        <Image url={`https://farm${r.farm}.staticflickr.com/${r.server}/${r.id}_${r.secret}.jpg`} key={r.id} title={r.title} />)
+
+        return (
+          <ul>
+            {img}
+          </ul>
+        )
+    }
+}
+
+export default Gallery;
