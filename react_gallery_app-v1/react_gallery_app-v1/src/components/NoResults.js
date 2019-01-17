054d667ffdf93e1127dcc08468ec7dcf0b81c7b3
Myles Y <39877233+mylesz123@users.noreply.github.com>
2019-01-17 13:27:31 -0500

    make NoResult component

---
+++ react_gallery_app-v1/react_gallery_app-v1/src/components/NoResults.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+const NoResults = (props) => {
+  return (
+    <div>
+      <i className='smh'></i>
+      <h3>We don't have that peice in our Gallery, Feel free to browse our other collections</h3>
+    </div>
+  )
+}
+
+export default NoResults;
